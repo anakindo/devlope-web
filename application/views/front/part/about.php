06095d5f25dbe7590d8cc34fe529c7b1f5889e45
anakindo <abdasyauqii74@gmail.com>
2017-08-19 23:50:57 +0700

    updet

---
+++ application/views/front/part/about.php
@@ -0,0 +1,102 @@
+<!-- Start : Slider -->
+    <div class="slider-area onepage_slider" id="page-top">
+        <div class="bend niceties preview-2">
+            <div class="slides" id="ensign-nivoslider">
+                <!-- slider image-->
+                <img alt="image" src="<?php echo base_url() ?>assets/images/slider/lp2.jpg" title="#slider-direction-1"> <img alt="image" src="<?php echo base_url() ?>assets/images/slider/slider2.jpg" title="#slider-direction-1">
+            </div>
+            <!-- end : slider image-->
+            <div class="t-cn slider-direction" id="slider-direction-1">
+                <div class="slider-progress"></div>
+                <div class="slider-content t-cn s-tb slider-1">
+                    <!-- slider content-->
+                    <div class="title-container s-tb-c title-compress">
+                        <div class="tp-caption small-content customin customout rs-title-small bg-sld-cp-primary tp-resizeme rs-parallaxlevel-0 wow zoomInDown" data-wow-delay="0.2s" data-wow-duration="2s">
+                            Welcome to BARRY
+                        </div>
+
+                        <div class="tp-caption big-title rs-title customin customout bg-sld-cp-primary wow fadeInDownBig">
+                            <span> Onepage </span> <span class="slide_st1"> Studio </span>
+                        </div>
+
+                    </div>
+                    <div class="button tp-caption customin rs-parallaxlevel-0 wow zoomInUp" data-wow-delay="0.3s" data-wow-duration="3s">
+                        <a href="#" class="more-link">Send request</a>
+                    </div>
+                </div>
+                <!-- End: slider content-->
+            </div>
+        </div>
+    </div>
+    <!-- End:  slider -->  <!-- ::::::::::::::::::::::::::: Start:About Section ::::::::::::::::::::::::::: -->
+    <section class="about-section" id="about">
+        <div class="container">
+            <div class="base-header">
+                <h3>Welcome to BERRY</h3>
+                <div class="abt_separator"></div>
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+            </div>
+
+            <div class="row">
+
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-srv">
+                        <div class="abt_icon">
+                            <i class="icon-heart"></i>
+                        </div>
+                        <div class="abt_content">
+                            <h3>Free support</h3>
+                            <div class="abt_separator"></div>
+                            <p>
+                                Lorem Ipsum is simply dummy text the printing and type setting orem Ipsum has been the industry
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-srv">
+                        <div class="abt_icon">
+                            <i class="icon-camera"></i>
+                        </div>
+                        <div class="abt_content">
+                            <h3>Responsive Design</h3>
+                            <div class="abt_separator"></div>
+                            <p>
+                                Lorem Ipsum is simply dummy text the printing and type setting orem Ipsum has been the industry
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-srv">
+                        <div class="abt_icon">
+                            <i class="icon-lightbulb"></i>
+                        </div>
+                        <div class="abt_content">
+                            <h3>Push Notifications</h3>
+                            <div class="abt_separator"></div>
+                            <p>
+                                Lorem Ipsum is simply dummy text the printing and type setting orem Ipsum has been the industry
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="single-srv">
+                        <div class="abt_icon">
+                            <i class="icon-strategy"></i>
+                        </div>
+                        <div class="abt_content">
+                            <h3>Ultra responsive</h3>
+                            <div class="abt_separator"></div>
+                            <p>
+                                Lorem Ipsum is simply dummy text the printing and type setting orem Ipsum has been the industry
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/ container -->
+    </section>
+    <!-- ::::::::::::::::::::::::::: End:About Section ::::::::::
---
+++ application/views/front/part/service.php
@@ -0,0 +1,108 @@
+  <!-- ::::::::::::::::::::::::::: Start:Our Services Section  ::::::::::::::::::::::::::: -->
+    <section class="about_desc">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5">
+                    <h3>Get all the Services</h3>
+                    <h4>with BARRY theme</h4>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. As doloribus ad dolore ipsum dolor sit amet quod ipsum eos aspernatur
+                        <strong>quod ipsums aspernatur vero eum magnam</strong> Lorem ipsum dolor sit amconsectet adipisicing elit. As doloribus ad dolore qui pariatur.
+                    </p>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit <strong>. As doloribus ad dolore qui pariatur </strong> quod ipsum eos aspernatur vero eum magna error non quasi repudiandae nostrum corrupti Lorem ipsum dolor sit a consectetur adipisicing elit. As doloribus ad dolore qui pariatur quod ipsum eos aspernatur
+                    </p>
+                </div>
+                <div class="col-md-7">
+                    <img src="<?php echo base_url() ?>assets/images/about/img_1.jpg" alt="about">
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- ::::::::::::::::::::::::::: End:Our Services Section  ::::::::::::::::::::::::::: -->
+
+
+
+    <!-- ::::::::::::::::::::::::::: Start:Our FEATURES Section  ::::::::::::::::::::::::::: -->
+    <section class="feature-section" id="service">
+        <div class="container">
+            <!-- Start: Heading -->
+            <div class="base-header">
+                <h3>OUR SERVICES</h3>
+                <div class="abt_separator"></div>
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+            </div>
+            <!-- End: Heading -->
+            <div class="row">
+                <div class="col-sm-4 col-xs-12">
+                    <div class="services-box-2">
+                        <div class="services_warp">
+                            <img src="<?php base_url() ?>assets/images/services/s1.jpg" alt="" />
+                            <a href="service.html">View Service</a>
+                        </div>
+                        <div class="number">01.</div>
+                        <h4>Digital Marketing</h4>
+                        <div class="subtext">Creative branding Ipsum is simply the dummy text of the printing and typesetting indust on Lorem Ipsum has been the industry's standard</div>
+                    </div>
+                </div>
+                <!-- End : Service Item One -->
+                <div class="col-sm-4 col-xs-12">
+                    <div class="services-box-2">
+                        <div class="services_warp">
+                            <img src="<?php echo base_url() ?>assets/images/services/s2.jpg" alt="" />
+                            <a href="service.html">View Service</a>
+                        </div>
+                        <div class="number">02.</div>
+                        <h4>Software Service</h4>
+                        <div class="subtext">Creative branding Ipsum is simply the dummy text of the printing and typesetting indust on Lorem Ipsum has been the industry's standard</div>
+                    </div>
+                </div>
+                <!-- End : Service Item Two -->
+                <div class="col-sm-4 col-xs-12">
+                    <div class="services-box-2">
+                        <div class="services_warp">
+                            <img src="<?php echo base_url() ?>assets/images/services/s3.jpg" alt="" />
+                            <a href="service.html">View Service</a>
+                        </div>
+                        <div class="number">03.</div>
+                        <h4>Bootstrap Theme</h4>
+                        <div class="subtext">Creative branding Ipsum is simply the dummy text of the printing and typesetting indust on Lorem Ipsum has been the industry's standard</div>
+                    </div>
+                </div>
+                <!-- End : Service Item Three -->
+            </div>
+            <!--/ row -->
+        </div>
+        <!--/ container -->
+    </section>
+    <!-- ::::::::::::::::::::::::::: End:Our Services Section  ::::::::::::::::::::::::::: -->
+   <!-- ::::::::::::::::::::::::::: Start: Call to action Section   ::::::::::::::::::::::::::: -->
+    <section class="calltoaction">
+        <div class="container">
+            <div class="widget-content">
+                <div class="row">
+                    <div class="col-lg-9 callto-pull-left">
+                        <div class="callto-icon">
+                            <span class="icon_cloud-download"></span>
+                        </div>
+                        <div class="callto-text">
+                            <h4>
+                                want to repair <span class="callto-hover"> your  Smartphone?</span>
+                            </h4>
+                            <p>
+                                We're here to help you manage your problem.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 callto-link">
+                        <a href="#" class="more-link">Send Request</a>
+                    </div>
+                </div>
+                <!--/ row -->
+            </div>
+            <!--/ widget -->
+        </div>
+        <!--/ container -->
+    </section>
+    <!-- ::::::::::::::::::::::::::: End: Call to action Section   ::::::::::::::::::::::::::: -->
