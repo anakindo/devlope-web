5a5ae2d8c0a4d5f01da11fde6a1f6efe853a8d46
anakindo <abdasyauqii74@gmail.com>
2017-08-19 23:59:25 +0700

    updategue

---
+++ application/views/front/part/blog.php
@@ -0,0 +1,68 @@
+ <!-- ::::::::::::::::::::::::::: Start: Blog Section  ::::::::::::::::::::::::::: -->
+    <section class="blog-section">
+        <div class="container">
+            <!-- Start: Heading -->
+            <div class="base-header">
+                <h3>Latest News</h3>
+                <div class="abt_separator"></div>
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type </p>
+            </div>
+            <!-- End: Heading -->
+            <div class="row">
+                <div class="col-sm-4 col-xs-12">
+                    <div class="blog-warp-1">
+                        <div class="blog_imgg">
+                            <img src="<?php echo base_url() ?>assets/images/blog/b1.jpg" alt="" />
+                            <div class="blog_datee">
+                                <p><i class="fa fa-calendar"></i> October 27, 2016 </p>
+                            </div>
+                        </div>
+                        <h5><a href="blog.html"> Operating System Updates</a></h5>
+                        <a href="#" class="subtext"><i class="fa fa-tag"></i>  Marketing </a> 
+                        <a href="#" class="subtext"><i class="fa fa-comment-o"> </i> 5 Comments </a>
+                        <p>Lorem Ipsum is simply dummy text ofthe anadthe printing of typesetting industrydummy texat the dummy text ofthe anadthe printing </p>
+                        <a href="single-blog.html" class="blog_btn">Read More <span class="fa fa-angle-double-right"></span></a>
+                    </div>
+                </div>
+                <!--/ Blog Item 1 --> 
+                <div class="col-sm-4 col-xs-12">
+                    <div class="blog-warp-1">
+                        <div class="blog_imgg">
+                            <img src="<?php echo base_url() ?>assets/images/blog/b2.jpg" alt="" />
+                            <div class="blog_datee">
+                                <p><i class="fa fa-calendar"></i> October 17, 2015 </p>
+                            </div>
+                        </div>
+                        <h5><a href="blog.html">System Security Testing</a></h5>
+                        <a href="#" class="subtext"><i class="fa fa-tag"></i>  Marketing </a> 
+                        <a href="#" class="subtext"><i class="fa fa-comment-o"> </i> 5 Comments </a>
+                        <p>Lorem Ipsum is simply dummy text ofthe anadthe printing of typesetting industrydummy texat the dummy text ofthe anadthe printing </p>
+                        <a href="single-blog.html" class="blog_btn">Read More <span class="fa fa-angle-double-right"></span></a>
+                    </div>
+                </div>
+                <!--/ Blog Item 2 -->
+                <div class="col-sm-4 col-xs-12">
+                    <div class="blog-warp-1">
+                        <div class="blog_imgg">
+                            <img src="<?php echo base_url() ?>assets/images/blog/b3.jpg" alt="" />
+                            <div class="blog_datee">
+                                <p><i class="fa fa-calendar"></i> October 24, 2014 </p>
+                            </div>
+                        </div>
+                        <h5><a href="blog.html">Operating System Updates</a></h5>
+                        <a href="#" class="subtext"><i class="fa fa-tag"></i>  Marketing </a> 
+                        <a href="#" class="subtext"><i class="fa fa-comment-o"> </i> 5 Comments </a>
+                        <p>Lorem Ipsum is simply dummy text ofthe anadthe printing of typesetting industrydummy texat the dummy text ofthe anadthe printing </p>
+                        <a href="single-blog.html" class="blog_btn">Read More <span class="fa fa-angle-double-right"></span></a>
+                    </div>
+                </div>
+                <!--/ Blog Item 3 -->
+                <div class="more_blog">
+                    <a href="blog.html" class="more-link">See Blog</a>
+                </div>
+            </div>
+            <!--/ row -->
+        </div>
+        <!--/ container -->
+    </section>
+    <!-- ::::::::::::::::::::::::::: End: Blog Section  ::::::::::::::::::::::::::: -->
---
+++ application/views/front/part/client.php
@@ -0,0 +1,70 @@
+ <!-- ::::::::::::::::::::::::::: Start: Client Section  ::::::::::::::::::::::::::: -->
+    <div class="client-section" id="client-section">
+        <!-- Container -->
+        <div class="container">
+            <!-- Start: Heading -->
+            <div class="base-header">
+                <h3>OUR BEST PARTNER</h3>
+                <div class="abt_separator"></div>
+            </div>
+            <!-- End: Heading -->
+            <div class="row">
+                <!-- client list  -->
+                <div class="owl-carousel owl-theme col-md-12" id="client">
+                    <!-- client 1 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-1.png">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- client 2 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-2.png">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- client 3 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-3.png">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- client 4 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-4.png">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- client 5 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-5.png">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- client 6 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-6.png">
+                            </div>
+                        </div>
+                    </div>
+                    <!-- client 7 -->
+                    <div class="item row">
+                        <div class="col-md-12">
+                            <div class="client-box"><img alt="client" src="<?php echo base_url() ?>assets/images/client/client-7.png">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!--/ client list  -->
+            </div>
+            <!--/ row - -->
+        </div>
+        <!--/ Container - -->
+    </div>
+    <!-- ::::::::::::::::::::::::::: End:Client Section   ::::::::::::::::::::::::::: -->
